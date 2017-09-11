09ec2673a06674405c74673a38c867222daa59d5
phanikumar1210 <phanikumardadi.kumar@gmail.com>
2017-09-11 10:17:37 +0530

    Added Test Cases

--- default/jenkins.sh
+++ default/jenkins.sh
@@ -12,11 +12,11 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the L1icense.
 
 set -xe
 
-mvn clean appengine:update \
+mvn clean appengine:deploy \
   -Dappengine.additionalParams="--service_account_json_key_file=${GOOGLE_APPLICATION_CREDENTIALS}" \
   -Dappengine.appId="${GOOGLE_PROJECT_ID}" \
   -Dappengine.version="${GOOGLE_VERSION_ID}" \
---
+++ test/src/test/java/com/example/appengine/helloworld/HelloServletTest.java
@@ -0,0 +1,37 @@
+package com.example.appengine.helloworld;
+
+import org.testng.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junit.framework.Assert;
+
+public class HelloServletTest 
+{	
+	static WebDriver driver;
+	@BeforeClass
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+	}
+	@AfterClass
+	public static void cleanUp(){
+		driver.quit();		
+	}
+	@Test
+	public void test() 
+	{
+		try
+		{
+			System.out.println("Entered Testing environmenet");
+			driver.get("https://pubsub-1.appspot.com/");
+			Assert.assertEquals("PapaJohns", driver.getTitle());
+			Assert.fail("Not yet implemented");
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+		}
+	}
+}
